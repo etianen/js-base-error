1d642da250a201d64d2c9e94bab951140bac9364
RobinBuschmann <robinbuschmann@hotmail.de>
2017-07-26 14:40:50 +0200

    tests reverted

--- test/index-test.js
+++ test/index-test.js
@@ -1,42 +1,30 @@
 const expect = require("chai").expect;
-const DefaultBaseError = require("../lib/index")["default"];
-const NamedBaseError = require("../lib/index").BaseError;
+const BaseError = require("../lib/index")["default"];
 
 
 describe("BaseError", () => {
 
-    [
-        ["default export", DefaultBaseError],
-        ["named export", NamedBaseError]
-    ].forEach(([description, BaseError]) => {
+    class TestError extends BaseError {};
 
-        describe(description, () => {
+    const testError = new TestError("foo");
 
-            class TestError extends BaseError {}
+    it("allows subclassing", () => {
+        expect(testError).to.be.an.instanceOf(TestError);
+        expect(testError).to.be.an.instanceOf(BaseError);
+        expect(testError).to.be.an.instanceOf(Error);
+        expect(Error()).to.not.be.an.instanceOf(TestError);
+    });
 
-            const testError = new TestError("foo");
+    it("provides a stack trace", () => {
+        expect(testError.stack).to.be.a("string");
+    });
 
-            it("allows subclassing", () => {
-                expect(testError).to.be.an.instanceOf(TestError);
-                expect(testError).to.be.an.instanceOf(BaseError);
-                expect(testError).to.be.an.instanceOf(Error);
-                expect(Error()).to.not.be.an.instanceOf(TestError);
-            });
+    it("provides a message", () => {
+        expect(testError.message).to.equal("foo");
+    });
 
-            it("provides a stack trace", () => {
-                expect(testError.stack).to.be.a("string");
-            });
-
-            it("provides a message", () => {
-                expect(testError.message).to.equal("foo");
-            });
-
-            it("provides a toString() implementation", () => {
-                expect(testError.toString()).to.equal("Error: foo");
-            });
-
-        });
-
+    it("provides a toString() implementation", () => {
+        expect(testError.toString()).to.equal("Error: foo");
     });
 
 });
